0813fddafce38d19fe51c28076d8596aa5df1d24
asteriskman <asteriskman@gmail.com>
2026-03-14 20:14:17 -0400

    get images behind fractals, switch julias properly, add import/export

--- index.js
+++ index.js
@@ -19,12 +19,10 @@
   min possible iterations per julia: 262144
 
   TODO:
-  add import/export
-  need to save progress when cursor moves
-  need to pick up progress when cursor moves
   sort out drawing or not drawing cell borders
-  get howie and julia faces into set space
   add win condition and display
+  change help references to "black pixel" since the set pixels are now
+    filled with howie and julia images
 
   accumulate points to increase iter rate
   write post text
@@ -110,7 +108,6 @@ class App {
     for (let x = 0; x < 32; x++) {
       for (let y = 0; y < 32; y++) {
         const grid = {
-          banked: 0,
           progress: 0, //progress goes from 0 to 512 * 512
           lastTime: 0,
           iters: 0
@@ -141,6 +138,70 @@ class App {
     window.location.reload();
   }
 
+  import() {
+    const importString = this.UI.imexText.value.trim();
+    if (importString.length % 8 !== 0) {
+      console.error("Corrupted import string. Must be multiple of 8 characters long.");
+      return;
+    }
+    const decodedStr = this.decodeExportStr(importString);
+    let state;
+    try {
+      state = JSON.parse(decodedStr);
+    } catch (error) {
+      console.error("Corrupted import string. JSON.parse check failed.");
+      return;
+    }
+
+    this.disableSaves = true;
+    localStorage.setItem(this.storageKey, decodedStr);
+    window.location.reload();  
+  }
+
+  genExportStr() {
+    this.saveToStorage();
+
+    const saveString = localStorage.getItem(this.storageKey);
+    const compressArray = LZString.compressToUint8Array(saveString);
+    const exportChars = 'howiemandeljulialouisdreyfus'.split``;
+    let exportArray = new Array(compressArray.length * 8);
+    for (let i = 0; i < compressArray.length; i++) {
+      const val = compressArray[i];
+      for (let b = 7; b >= 0; b--) {
+        const bit = (val & (1 << b)) >> b;
+        const cif = (i * 8 + (7 - b)) 
+        const ci = cif % exportChars.length;
+        const c = (bit === 1) ? exportChars[ci].toUpperCase() : exportChars[ci];
+        exportArray[cif] = c;
+      }
+    }
+
+    return exportArray.join``;
+  }
+
+  decodeExportStr(str) {
+    const arraySize = Math.round(str.length / 8);
+    const compressArray = new Uint8Array(arraySize);
+    
+    for (let i = 0; i < arraySize; i++) {
+      let val = 0;
+      for (let b = 7; b >=0; b--) {
+        const cif = i * 8 + (7 - b);
+        const c = str[cif];
+        const bit = c === c.toUpperCase() ? 1 : 0;
+        val = val | (bit << b);
+      }
+      compressArray[i] = val;
+    }
+
+    const saveString = LZString.decompressFromUint8Array(compressArray);
+    return saveString;    
+  }
+
+  export() {
+    this.UI.imexText.value = this.genExportStr();
+  }
+
   initUI() {
     this.UI = {};
 
@@ -156,6 +217,11 @@ class App {
     this.UI.resetNo.onclick = () => this.closeModal('resetConfirm');
     this.UI.helpClose.onclick = () => this.closeModal('helpContainer');
     this.UI.showHelp.onclick = () => this.showModal('helpContainer');
+    this.UI.impExp.onclick = () => this.showModal('imexContainer');
+    this.UI.imexImport.onclick = () => this.import();
+    this.UI.imexExport.onclick = () => this.export();
+    this.UI.imexClose.onclick = () => this.closeModal('imexContainer');
+
 
     this.cmain.onclick = (evt) => this.onclick(evt);
     this.UI.showMap.onclick = () => this.showMap();
@@ -288,13 +354,22 @@ class App {
   }
 
   drawMandel(ctx, locx, locy, size, step) {
+    ctx.drawImage(this.UI.imgHM1, locx, locy, size, size, locx, locy, size, size);
     for (let cx = locx; cx < locx + size; cx += step) {
       for (let cy = locy; cy < locy + size; cy += step) {
         const x = this.lerp(-2, 1, cx / ctx.canvas.width);
         const y = this.lerp(-1.5, 1.5, cy / ctx.canvas.width);
         const val = this.getMandel(x, y, 100);
-        const rgb = this.hslToRgb(val * 360 / 100, 0.5, val === 0 ? 0 : 0.5);
-        this.webgl.addRect(cx, cy, step, step, rgb.r, rgb.g, rgb.b, 1);
+        let a;
+        let rgb;
+        if (val === 0) {
+          rgb = {r: 0, g: 0, b: 0};
+          a = 0;
+        } else {
+          rgb = this.hslToRgb(val * 360 / 100, 0.5, val === 0 ? 0 : 0.5);
+          a = 1;
+        }
+        this.webgl.addRect(cx, cy, step, step, rgb.r, rgb.g, rgb.b, a);
       }
     }
   }
@@ -304,8 +379,12 @@ class App {
   }
 
   drawJulia(ctx, locx, locy, size, step, cr, ci, state) {
+    const big = size > 32;
     const statef = state.progress / this.maxProgress;
     const iw = size / step;
+    if (big) {
+      ctx.drawImage(this.UI.imgJLD1, locx, locy, size, size, locx, locy, size, size); 
+    }
     for (let yi = 0; yi < iw; yi++) {
       for (let xi = 0; xi < iw; xi++) {
         const cx = xi * step;
@@ -316,12 +395,25 @@ class App {
         //TODO: get julia data from calc
         const val = this.getJulia(x, y, cr, ci, 100);
         let rgb;
+        let a = 1;
         if (statef > f) {
-          rgb = this.hslToRgb(val * 360 / 100, 0.5, val === 0 ? 0: 0.5);
+          if (val === 0) {
+            rgb = {r: 0, g: 0, b: 0};
+            if (big) {
+              a = 0;
+            } else {
+              a = 1;
+            }
+          } else {
+            rgb = this.hslToRgb(val * 360 / 100, 0.5, val === 0 ? 0: 0.5);
+            a = 1;
+          }
+
         } else {
           rgb = {r: 0.5, g: 0.5, b: 0.5};
+          a = 1;
         }
-        this.webgl.addRect(cx + locx, cy + locy, step, step, rgb.r, rgb.g, rgb.b, 1);
+        this.webgl.addRect(cx + locx, cy + locy, step, step, rgb.r, rgb.g, rgb.b, a);
       }
     }
   }
@@ -349,6 +441,7 @@ class App {
 
   tick() {
     this.rate = 10 + 990 * Math.pow(this.state.setPoints / 6656596, 0.5);
+    //this.rate = 10000;
     this.juliaPercent = 0; //TODO: calc this
     this.mandelPercent = 0; //TODO: calc this
     this.juliaMSRem = 0;
@@ -368,6 +461,8 @@ class App {
         const curTime = (new Date()).getTime();
         const deltaTime = gridStatus.lastTime > 0 ? ((curTime - gridStatus.lastTime) / 1000) : 0;
         let itersRemaining = this.rate * deltaTime;
+        //TODO: figure out if large deltaTime from having the game closed for a while will
+        //      be so much that this will freeze too long
         while (itersRemaining > 0 && gridStatus.progress < this.maxProgress) {
           const oldIters = gridStatus.iters;
           gridStatus.iters += itersRemaining;
@@ -381,6 +476,7 @@ class App {
         }
         gridStatus.lastTime = curTime;
         this.juliaMSRem = 1000 * this.juliaRem / this.rate;
+        this.juliaPercent = 100 * (1 - this.juliaRem / this.juliaTotal);
 
         if (gridStatus.progress >= this.maxProgress) {
           //draw mandel
@@ -399,7 +495,7 @@ class App {
               this.ctx.drawImage(this.webgl.canvas, 0, 0, 1024, 1024, 0, 0, 1024, 1024);
             }
           }
-          this.state.marker++;
+          //this.state.marker++;
           this.juliaData = undefined;
         } else {
           if (this.level !== -1 ) {
@@ -444,7 +540,10 @@ class App {
         this.webgl.draw();
         this.ctx.drawImage(this.webgl.canvas, 0, 0, 1024, 1024, 0, 0, 1024, 1024);
       } else {
+        console.log("CHANGE GRID");
+        this.state.gridStatus[this.state.marker].lastTime = 0;
         this.state.marker = clickedLevel;
+        this.state.gridStatus[this.state.marker].lastTime = 0;
       }
     } else {
       this.showMap();
@@ -515,3 +614,37 @@ class App {
 }
 
 const app = new App();
+
+
+/*
+Below is pieroxy's LZString and license
+*/
+
+/*
+MIT License
+
+Copyright (c) 2013 pieroxy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+var LZString=function(){var r=String.fromCharCode,o="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",n="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-$",e={};function t(r,o){if(!e[r]){e[r]={};for(var n=0;n<r.length;n++)e[r][r.charAt(n)]=n}return e[r][o]}var i={compressToBase64:function(r){if(null==r)return"";var n=i._compress(r,6,function(r){return o.charAt(r)});switch(n.length%4){default:case 0:return n;case 1:return n+"===";case 2:return n+"==";case 3:return n+"="}},decompressFromBase64:function(r){return null==r?"":""==r?null:i._decompress(r.length,32,function(n){return t(o,r.charAt(n))})},compressToUTF16:function(o){return null==o?"":i._compress(o,15,function(o){return r(o+32)})+" "},decompressFromUTF16:function(r){return null==r?"":""==r?null:i._decompress(r.length,16384,function(o){return r.charCodeAt(o)-32})},compressToUint8Array:function(r){for(var o=i.compress(r),n=new Uint8Array(2*o.length),e=0,t=o.length;e<t;e++){var s=o.charCodeAt(e);n[2*e]=s>>>8,n[2*e+1]=s%256}return n},decompressFromUint8Array:function(o){if(null==o)return i.decompress(o);for(var n=new Array(o.length/2),e=0,t=n.length;e<t;e++)n[e]=256*o[2*e]+o[2*e+1];var s=[];return n.forEach(function(o){s.push(r(o))}),i.decompress(s.join(""))},compressToEncodedURIComponent:function(r){return null==r?"":i._compress(r,6,function(r){return n.charAt(r)})},decompressFromEncodedURIComponent:function(r){return null==r?"":""==r?null:(r=r.replace(/ /g,"+"),i._decompress(r.length,32,function(o){return t(n,r.charAt(o))}))},compress:function(o){return i._compress(o,16,function(o){return r(o)})},_compress:function(r,o,n){if(null==r)return"";var e,t,i,s={},u={},a="",p="",c="",l=2,f=3,h=2,d=[],m=0,v=0;for(i=0;i<r.length;i+=1)if(a=r.charAt(i),Object.prototype.hasOwnProperty.call(s,a)||(s[a]=f++,u[a]=!0),p=c+a,Object.prototype.hasOwnProperty.call(s,p))c=p;else{if(Object.prototype.hasOwnProperty.call(u,c)){if(c.charCodeAt(0)<256){for(e=0;e<h;e++)m<<=1,v==o-1?(v=0,d.push(n(m)),m=0):v++;for(t=c.charCodeAt(0),e=0;e<8;e++)m=m<<1|1&t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;e<h;e++)m=m<<1|t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=c.charCodeAt(0),e=0;e<16;e++)m=m<<1|1&t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}0==--l&&(l=Math.pow(2,h),h++),delete u[c]}else for(t=s[c],e=0;e<h;e++)m=m<<1|1&t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;0==--l&&(l=Math.pow(2,h),h++),s[p]=f++,c=String(a)}if(""!==c){if(Object.prototype.hasOwnProperty.call(u,c)){if(c.charCodeAt(0)<256){for(e=0;e<h;e++)m<<=1,v==o-1?(v=0,d.push(n(m)),m=0):v++;for(t=c.charCodeAt(0),e=0;e<8;e++)m=m<<1|1&t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}else{for(t=1,e=0;e<h;e++)m=m<<1|t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t=0;for(t=c.charCodeAt(0),e=0;e<16;e++)m=m<<1|1&t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t>>=1}0==--l&&(l=Math.pow(2,h),h++),delete u[c]}else for(t=s[c],e=0;e<h;e++)m=m<<1|1&t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;0==--l&&(l=Math.pow(2,h),h++)}for(t=2,e=0;e<h;e++)m=m<<1|1&t,v==o-1?(v=0,d.push(n(m)),m=0):v++,t>>=1;for(;;){if(m<<=1,v==o-1){d.push(n(m));break}v++}return d.join("")},decompress:function(r){return null==r?"":""==r?null:i._decompress(r.length,32768,function(o){return r.charCodeAt(o)})},_decompress:function(o,n,e){var t,i,s,u,a,p,c,l=[],f=4,h=4,d=3,m="",v=[],g={val:e(0),position:n,index:1};for(t=0;t<3;t+=1)l[t]=t;for(s=0,a=Math.pow(2,2),p=1;p!=a;)u=g.val&g.position,g.position>>=1,0==g.position&&(g.position=n,g.val=e(g.index++)),s|=(u>0?1:0)*p,p<<=1;switch(s){case 0:for(s=0,a=Math.pow(2,8),p=1;p!=a;)u=g.val&g.position,g.position>>=1,0==g.position&&(g.position=n,g.val=e(g.index++)),s|=(u>0?1:0)*p,p<<=1;c=r(s);break;case 1:for(s=0,a=Math.pow(2,16),p=1;p!=a;)u=g.val&g.position,g.position>>=1,0==g.position&&(g.position=n,g.val=e(g.index++)),s|=(u>0?1:0)*p,p<<=1;c=r(s);break;case 2:return""}for(l[3]=c,i=c,v.push(c);;){if(g.index>o)return"";for(s=0,a=Math.pow(2,d),p=1;p!=a;)u=g.val&g.position,g.position>>=1,0==g.position&&(g.position=n,g.val=e(g.index++)),s|=(u>0?1:0)*p,p<<=1;switch(c=s){case 0:for(s=0,a=Math.pow(2,8),p=1;p!=a;)u=g.val&g.position,g.position>>=1,0==g.position&&(g.position=n,g.val=e(g.index++)),s|=(u>0?1:0)*p,p<<=1;l[h++]=r(s),c=h-1,f--;break;case 1:for(s=0,a=Math.pow(2,16),p=1;p!=a;)u=g.val&g.position,g.position>>=1,0==g.position&&(g.position=n,g.val=e(g.index++)),s|=(u>0?1:0)*p,p<<=1;l[h++]=r(s),c=h-1,f--;break;case 2:return v.join("")}if(0==f&&(f=Math.pow(2,d),d++),l[c])m=l[c];else{if(c!==h)return null;m=i+i.charAt(0)}v.push(m),l[h++]=i+m.charAt(0),i=m,0==--f&&(f=Math.pow(2,d),d++)}}};return i}();"function"==typeof define&&define.amd?define(function(){return LZString}):"undefined"!=typeof module&&null!=module?module.exports=LZString:"undefined"!=typeof angular&&null!=angular&&angular.module("LZString",[]).factory("LZString",function(){return LZString});
+
+
+
